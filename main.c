491aeae40083f68e3bd264496f16786fc67b0978
moa <moa.1991@gmail.com>
2011-12-07 19:35:48 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  pintOS
+//
+//  Created by Lucas Severyn on 07/12/2011.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
