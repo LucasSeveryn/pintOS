1a2621c3b21b5c5981b777d6289e43d6f053123e
Robert Kruszewski <dzobert@gmail.com>
2012-02-02 23:24:15 +0000

    initial thread_set_priority, does not include priority scheduling

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -362,7 +362,12 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->priority = new_priority;
+  struct thread *t = thread_current ();
+  t->base_priority = new_priority;
+  if(new_priority > t->priority)
+  {
+  	t->priority = new_priority;
+  }
 }
 
 /* Returns the current thread's priority. */
