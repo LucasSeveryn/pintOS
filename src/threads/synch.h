221249f2811e0c5cdf823fabcc6127daaf3df7bf
Robert Kruszewski <dzobert@gmail.com>
2012-02-02 23:36:12 +0000

    added function prototypes to synch.h from synch.c for recomputing priorities

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -31,6 +31,10 @@ bool lock_try_acquire (struct lock *);
 void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
 
+/* Functions for recomputing priorities */
+void donate_priority (const struct lock *);
+void recompute_priority ();
+
 /* Condition variable. */
 struct condition 
   {
