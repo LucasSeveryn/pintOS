89d1322268682c7db1db4fe920ab058345036767
Robert Kruszewski <dzobert@gmail.com>
2012-02-02 23:23:18 +0000

    new lock field which allows embedding it in a list

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -22,6 +22,7 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+	struct list_elem lock_elem; /* List element for held locks by thread. */
   };
 
 void lock_init (struct lock *);
