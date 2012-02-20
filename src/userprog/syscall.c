3e60c51063023de664252bf13ddead817780ba7f
Lukasz Koprowski <azram19@gmail.com>
2012-02-20 17:36:44 +0000

    ignore

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -45,11 +45,30 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  //syscall_noa[ SYS_EXIT ] = 0;
+
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
+
+  //void * args = syscall_retrieve_args(f);
+  //int noa = (int) args[0];
+	//syscall_functions( noa );
+  
   thread_exit ();
 }
+/*
+static void *
+syscall_retrieve_args(struct intr_frame *f){
+	void * args = new (void*)[3];
+	
+	return args;
+} 
+
+static int 
+exit( void * args ){
+	
+}*/
