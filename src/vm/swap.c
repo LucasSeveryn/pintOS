875275d38e880fbd77d472985834bb94852528e2
Lukasz Seweryn <moa.1991@gmail.com>
2012-03-18 22:51:17 +0000

    ...

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -4,9 +4,12 @@
 /* List of free swap slots. */
 static struct list free_swap_list;
 
+/* Swap partition */
+static struct disk *swap_parition;
+
 /* Bitmap of free swap slots. */
 static struct bitmap free_swap_bitmap;
 
 void swap_init(){
-	
+	//bitmap_init(&free_swap_bitmap);
 }
